cfd8f895142b005c2d762ff43e66d9914800045c
Pete <not.gonna.tell@you.com>
2021-08-12 19:49:40 +0200

    Added option to supply a variable for OpenCV Config

--- cmake/UseOpenCV.cmake
+++ cmake/UseOpenCV.cmake
@@ -2,11 +2,13 @@
 # UseOpenCV.cmake #
 ###################
 find_package(OpenCV 4.0.0 QUIET)
+find_package(OpenCV 4.0.0 QUIET CONFIG PATHS ${OpenCV_CONFIG_DIR})
 OPTION(WITH_OPENCV "Build with OpenCV support?" ${OpenCV_FOUND})
 
 IF(WITH_OPENCV)
     MESSAGE("WITH_OPENCV: " ${WITH_OPENCV})
   IF(NOT OpenCV_FOUND)
+      MESSAGE("OpenCV was not found, it will be downloaded and build externally. If you have installed OpenCV already, set the OpenCV_CONFIG_DIR variable to the directory containing the OpenCVConfig.cmake")
       INCLUDE(ConfigureTimeDependency)
       add_configure_time_dependency(OpenCV)
       find_package(OpenCV REQUIRED)
